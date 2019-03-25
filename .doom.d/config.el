e6b6cda51aaa361bb4083e49ea5504f871c8cf71
Marc Carné <marc.carne@werlabs.com>
2019-03-25 01:33:39 +0100

    Fix treemacs, change theme, change font

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,3 +1,14 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
+
+;; Set global fonts
+(add-to-list 'default-frame-alist '(font . "Ubuntu Mono 14" ))
+(set-face-attribute 'default t :font "Ubuntu Mono 14" )
+
+;; Set theme
+(load-theme 'doom-one-light t)
+
+;; Reset treemacs icons
+(with-eval-after-load 'treemacs
+  (treemacs-reset-icons))
